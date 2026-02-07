5be7289ae3c5bf01096b35b001b2cb8d4de8f323
ajayhanssen <ta2244@mci4me.at>
2026-02-07 19:38:52 +0100

    mehr fixes

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 import numpy as np
 
-def main():
-    x = np.linspace(0,100,100)
+import time
+import tracemalloc
+import functools
 
-    print(x.T@x)
+def timedit(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        start_time = time.time()
+
+        result = func(*args, **kwargs)
+
+        end_time = time.time()
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        print(
+            f"Function '{func.__name__}' executed in "
+            f"{end_time - start_time:.4f}s | "
+            f"Peak memory: {peak / 1024 / 1024:.2f} MB"
+        )
+
+        return result
+    return wrapper
+
+@timedit
+def functen(A,B):
+    return A@B
+
+def main():
+    x = np.ones((10,10))
+
+    A = np.ones((100,100))
+    B = np.ones((100,100))*2
+    functen(A,B)
+
+
     
 
 
